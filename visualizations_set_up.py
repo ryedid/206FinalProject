a99eea6cc642de70a6f750cc1a3165e979885b8b
ryedid <124733109+ryedid@users.noreply.github.com>
2023-12-04 14:07:00 -0500

    Create visualizations_set_up.py
    woohoo we see stuff

---
+++ visualizations_set_up.py
@@ -0,0 +1,50 @@
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def fetch_weather_data_from_db(database_path):
+    conn = sqlite3.connect(database_path)
+    query = "SELECT city, date, hour, temp FROM WeatherCities"
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df
+
+def plot_temperature_by_city(df):
+    # Convert date and hour columns to a datetime format
+    df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['hour'])
+
+    # Group by city and date, then plot temperature over time
+    for city, data in df.groupby('city'):
+        data.set_index('datetime')['temp'].plot(label=city, figsize=(10, 6))
+
+    plt.title('Temperature by City Over Time')
+    plt.xlabel('Date')
+    plt.ylabel('Temperature (°F)')
+    plt.legend()
+    plt.show()
+
+def fetch_weather_bike_data(database_path):
+    conn = sqlite3.connect(database_path)
+    # Select necessary columns from both tables and perform a join
+    query = """
+        SELECT wc.city, wc.precip, bc.free_bikes
+        FROM WeatherCities wc
+        JOIN BikeCities bc ON wc.city = bc.city
+    """
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df
+def plot_bikes_precip_scatter(df):
+    plt.scatter(df['precip'], df['free_bikes'])
+    plt.title('Number of Free Bikes vs Precipitation Prediction')
+    plt.xlabel('Precipitation Prediction (%)')
+    plt.ylabel('Number of Free Bikes')
+    plt.show()
+
+
+weather_data = fetch_weather_data_from_db("proj_base")
+# Plot temperature trends by city
+plot_temperature_by_city(weather_data)
+weather_bike_data = fetch_weather_bike_data("proj_base")
+plot_bikes_precip_scatter(weather_bike_data)
+
