3e154c6d8e7cf10fe8640cf6e2d105ef04f32c08
ryedid <124733109+ryedid@users.noreply.github.com>
2023-12-04 21:29:54 -0500

    Update city_data.py

--- city_data.py
+++ city_data.py
@@ -87,7 +87,41 @@ def make_SQL(data_list, cur, conn):
 
         
 
-    conn.commit()    
+    conn.commit()  
+def normalize_city(cur, conn):
+    # Define the SQL command to update the city column
+    sql_command = """
+        UPDATE CityHealth
+        SET city = 
+            CASE 
+                WHEN city LIKE 'San-Antonio%' THEN 'San Antonio, TX'
+                WHEN city LIKE 'Des-Moines%' THEN 'Des Moines, IA'
+                WHEN city LIKE 'Park-City%' THEN 'Park City, UT'
+                WHEN city LIKE 'Long-Beach%' THEN 'Long Beach, CA'
+                WHEN city LIKE 'Los-Angeles%' THEN 'Los Angeles, NV'
+                WHEN city LIKE 'Fort-Lauderdale%' THEN 'Fort Lauderdale, FL'
+                WHEN city LIKE 'El-Paso%' THEN 'El Paso, TX'
+                WHEN city LIKE 'New-York%' THEN 'New York, NY'
+                WHEN city LIKE 'Fort-Worth%' THEN 'Fort Worth, TX'
+                WHEN city LIKE 'Las-Vegas%' THEN 'Las Vegas, NV'
+                WHEN city LIKE 'Oklahoma-City%' THEN 'Oklahoma City, OK'
+                WHEN city LIKE '%San-Francisco%' THEN 'San Francisco Bay Area, CA'
+                WHEN city LIKE '%Salt-Lake-City-Utah%' THEN 'Salt Lake City'
+                WHEN city LIKE 'Kailua' THEN 'Kailua-Kona'
+                ELSE city
+            END;
+    """
+    try:
+        # Execute the SQL command
+        cur.execute(sql_command)
+        # Commit the changes to the database
+        conn.commit()
+        print("Normalization completed successfully!")
+    except Exception as e:
+        # Rollback changes if there's an error
+        conn.rollback()
+        print(f"Error during normalization: {str(e)}")
+
 def normalize_state(cur, conn):
     # Define the SQL command to update the city column
     sql_command = """
