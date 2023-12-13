e1ae9610a93278dc26b8bb6307922cd0b378ebfe
heldmane <97550323+heldmane@users.noreply.github.com>
2023-12-12 21:02:09 -0500

    Update city_bikes_part.py
    took out some comments

--- city_bikes_part.py
+++ city_bikes_part.py
@@ -16,22 +16,20 @@ def get_city_bike_data():
     Returns:
     - dict: Parsed JSON response from the API.
     """
-    # Replace 'YOUR_API_KEY' with your actual API key
+    
     base_url = "https://api.citybik.es/v2/"
     # endpoint = "networks"
     endpoint = "networks"
     url = f"{base_url}{endpoint}"
 
-    # Include your API key in the headers
+    
     # headers = {"Authorization": f"Bearer {api_key}"}
 
     try:
-        # Make a GET request to the API
+        
         response = requests.get(url)
 
-        # Check if the request was successful (status code 200)
         if response.status_code == 200:
-            # Parse the JSON response
             data = response.json()
             return data
         else:
@@ -40,7 +38,6 @@ def get_city_bike_data():
             return None
 
     except requests.exceptions.RequestException as e:
-        # Handle any exceptions that may occur during the request
         print(f"Error: {e}")
         return None
 
@@ -49,7 +46,7 @@ def load_json(data):
     filename = dir_path + '/' + "citybike.json"
     with open(filename, 'w') as json_file:
         json.dump(data, json_file, indent=2)
-# Replace 'YOUR_API_KEY' with your actual City Bike API key
+
 
 
 
@@ -109,28 +106,23 @@ def get_list_of_ids():
 
 def get_new_data(url):
 
-    # Replace 'YOUR_API_KEY' with your actual API key
+  
     base_url = url
 
-    # Include your API key in the headers
+   
     # headers = {"Authorization": f"Bearer {api_key}"}
 
     try:
-        # Make a GET request to the API
         response = requests.get(base_url)
 
-        # Check if the request was successful (status code 200)
         if response.status_code == 200:
-            # Parse the JSON response
             data = response.json()
             return data
         else:
-            # Print an error message if the request was not successful
             print(f"Error: {response.status_code} - {response.text}")
             return None
 
     except requests.exceptions.RequestException as e:
-        # Handle any exceptions that may occur during the request
         print(f"Error: {e}")
         return None
 
